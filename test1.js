8ec52c7541e903a1570156f4e6cd39d9c5169bea
kaviyarasu786 <83492728+kaviyarasu786@users.noreply.github.com>
2023-11-25 08:11:32 +0530

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,105 @@
+/*alert message*/
+alert("hello jk");
+
+/*console.log*/
+console.log("welocome");
+
+/*document.write*/
+document.write("This is new webpage........");
+
+/*Variables*/
+
+//addition
+var a=10;
+var b=23;
+var c=a+b;
+console.log(c);
+
+//subtraction
+var x=100;
+var y=20;
+var z=x-y;
+console.log(z);
+
+//multiplication
+var a=20;
+var b=2;
+var c=a*b;
+console.log(c);
+
+//let
+let e=2;
+let f=3;
+let h=e+f;
+console.log(h);
+
+//const
+const name="Rohit";
+console.log(name);
+
+const rollnumber=2332;
+console.log(rollnumber);
+
+const carname="Toyota";
+console.log(carname);
+alert(carname);
+document.write(carname);
+
+/*Data type*/
+
+//string
+var firstname="rohit";
+console.log(firstname);
+
+let gender="male";
+console.log(gender);
+
+const fathername="ganapathi";
+console.log(fathername);
+
+//integer
+
+var a=10;
+console.log(a);
+
+var x=2;
+var y=4;
+var c=x+y;
+console.log(c);
+//document.write(c);
+
+//boolean 
+
+var x=2;
+var y=4;
+var z=x<y;
+console.log(z);
+
+let g=5;
+let s=4;
+let d=g>s;
+console.log(d);
+
+const j=4;
+const k=33;
+const n=j<k;
+console.log(n);
+
+//undefined
+
+var t;
+console.log(t);
+
+let m;
+console.log(m);
+
+//Null
+
+var age=null;
+console.log(age);
+
+let moblienumber=null;
+console.log(moblienumber);
+
+let amount=1000;
+console.log("Your Bill amount is "+amount);
